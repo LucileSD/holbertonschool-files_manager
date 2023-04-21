71b4311139e5a35940c4476c303c1ac139f807a2
LucileSD <4316@holbertonschool.com>
2023-04-21 09:58:45 +0200

    userId

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,16 +1,17 @@
 import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
-import UsersController from './UsersController';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
-import { ObjectId } from 'mongodb';
 
 class FilesController {
   static async postUpload(request, response) {
     const token = request.headers['x-token'];
     const key = `auth_${token}`;
     let userIId = await redisClient.get(key);
-
+    if (!userIId) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
     userIId = userIId.slice(1, -1);
 
     const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userIId) });
@@ -56,17 +57,19 @@ class FilesController {
         name, type, parentId, isPublic, data,
       });
       newFile.insertedId;
+      const newValues = { $set: { userId } };
+      await dbClient.db.collection('files').updateOne(findFile, newValues);
       findFile = await dbClient.db.collection('files').findOne({ name });
       return response.status(201).send(findFile);
     }
     const path = process.env.FOLDER_PATH || '/tmp/files_manager';
     const folderName = `${path}/${uuidv4()}`;
-    if(!fs.existsSync(path)) {
-      fs.mkdirSync(path, {recurisve: true});
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path, { recurisve: true });
     }
 
     const content = Buffer.from(data, 'base64').toString('ascii');
-    fs.writeFile(folderName, content, err => {
+    fs.writeFile(folderName, content, (err) => {
       if (err) console.log(err);
     });
     await dbClient.db.collection('files').insertOne({
