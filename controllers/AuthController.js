c5957c4e8372f05d0b36292fcd40fd14ae870f23
LucileSD <4316@holbertonschool.com>
2023-04-25 11:20:00 +0200

    eslint

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,17 +1,17 @@
-import redisClient from "../utils/redis";
 import sha1 from 'sha1';
-import dbClient from "../utils/db";
 import { v4 as uuidv4 } from 'uuid';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AuthController {
   static async getConnect(request, response) {
     const base64 = request.headers.authorization.split(' ')[1];
     const credentials = Buffer.from(base64, 'base64').toString('ascii');
     const [email, password] = credentials.split(':');
-    const user = await dbClient.db.collection('users').findOne({ email, password:sha1(password) });
+    const user = await dbClient.db.collection('users').findOne({ email, password: sha1(password) });
 
     if (!user) {
-      return response.status(401).send({ error: 'Unauthorized'});
+      return response.status(401).send({ error: 'Unauthorized' });
     }
 
     const token = uuidv4();
@@ -24,8 +24,8 @@ class AuthController {
     const token = request.headers['x-token'];
     const key = `auth_${token}`;
     const userId = await redisClient.get(key);
-    if(!userId) {
-      return response.status(401).send({ error: 'Unauthorized'});
+    if (!userId) {
+      return response.status(401).send({ error: 'Unauthorized' });
     }
     await redisClient.del(key);
     return response.status(204);
